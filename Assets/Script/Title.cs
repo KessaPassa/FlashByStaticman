4597df2687ccb848ef50f588b4ca667595e8d8b1
KessaPassa <k.someya.gyao@gmail.com>
2016-09-28 23:28:34 +0900

    FadeManager追加

--- Assets/Script/Title.cs
+++ Assets/Script/Title.cs
@@ -1,15 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class Title : MonoBehaviour {
-
-	// Use this for initialization
+    private FadeManager fadeManager;
+	
 	void Start () {
-	
+        fadeManager = FindObjectOfType<FadeManager>();
 	}
 	
-	// Update is called once per frame
+	
 	void Update () {
-	
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Return))
+        {
+            fadeManager.FadeStart(1);
+        }
 	}
+
+    public void OnStaartButton()
+    {
+
+    }
 }
